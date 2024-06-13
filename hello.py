2a115a5fda29395ecf60611ead85ca32ed881d6d
Ankith Gunisetti <ankith.gunisetti@in.techwave.net>
2024-06-13 11:29:25 +0530

    Modified hello.py

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def hello_world():
-    return '<h1 style="color:#F333FF; text-align:center;"> This webpage is for practicing Git and Github</h1>'
+    return '<h1 style="color:#F333FF; text-align:center;"> This webpage is for practicing Git and Github with Udemy</h1>'
  
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
