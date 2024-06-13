a64f556898d14e9a21999a333698b70a1a2d571b
Bharath <bharath@hotmail.com>
2024-06-13 11:12:24 +0530

    uploading files

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello World! Welcome ")
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def hello_world():
+    return '<h1 style="color:#F333FF; text-align:center;"> This webpage is for practicing Git and Github</h1>'
+ 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
